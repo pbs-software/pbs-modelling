66c58c471bea63121c238a56d9165446cc61e646
alex_google <alex_google@mofo.ca>
2009-08-18 07:57:23 +0000

    created column grouping algorithm for aligning buttons to grid in order to minimize number of rows

--- present_talk.r
+++ present_talk.r
@@ -184,11 +184,11 @@ getIndexForSection <- function( section_id )
 getButton <- function( obj )
 {
 	if( inherits( obj, "section" ) ) {
-		b <- paste( "button text=\"", obj@name, "\" function=setsection action=\"", obj@section_id,"\" padx=4 pady=4 fg=red3 bg=whitesmoke",sep="")
+		b <- paste( "button text=\"", obj@name, "\" function=setsection action=\"", obj@section_id,"\" width=10 padx=4 pady=4 fg=red3 bg=whitesmoke",sep="")
 		return( b )
 	}
 	if( inherits( obj, "file" ) ) {
-		b <- paste( "button text=\"", obj@name, "\" function=presentTalkOpenFile action=\"", obj@filename,"\" padx=4 pady=4 fg=blue bg=whitesmoke",sep="")
+		b <- paste( "button text=\"", obj@name, "\" function=presentTalkOpenFile action=\"", obj@filename,"\" width=10 padx=4 pady=4 fg=blue bg=whitesmoke",sep="")
 		return( b )
 	}
 }
@@ -196,45 +196,76 @@ getButton <- function( obj )
 getButtons <- function( talk )
 {
 	#create a list of buttons
-	but <- list( list(), list(), list() )
+	section_but <- list()
+	file_but <- list()
 
 	#process top level buttons (under talk)
 	for( f in talk@files ) {
-		i <- length( but[[ f@col ]] ) + 1
-		but[[ f@col ]][[ i ]] <- f
+		i <- length( file_but ) + 1
+		file_but[[ i ]] <- f
 	}
 
 	#process buttons under sections
 	sect_id <- 1
 	for( s in talk@sections ) {
 		if( s@button == TRUE ) {
-			i <- length( but[[ s@col ]] ) + 1
+			i <- length( section_but ) + 1
 			s@section_id <- as.integer( sect_id )
-			but[[ s@col ]][[ i ]] <- s
+			section_but[[ i ]] <- s
 		}
 		for( item in s@items ) {
 			if( inherits( item, "file" ) && item@button == TRUE ) {
-				i <- length( but[[ item@col ]] ) + 1
-				but[[ item@col ]][[ i ]] <- item
+				i <- length( file_but ) + 1
+				file_but[[ i ]] <- item
 			}
 		}
 		sect_id <- sect_id + 1
 	}
 
 	#create win desc corresponding to list of buttons
-	w <- "grid 1 3 relief=groove sticky=NEW"
-	for( i in 1:3 ) {
-		l <- length( but[[ i ]] ) + 1
-		if( l == 0 ) {
-			w <- append( w, "null" )
-			next
+
+	n_files <- length( file_but ) #files on the left
+	n_sections <- length( section_but ) #sections on the right
+	cols <- 4 #number of cols to use
+	min_rows <- 9999
+	min_row_i <- 0
+	for( i in 1:cols ) {
+		#determine optimal column assignment
+		rows <- ceiling( max( n_files / i, n_sections / ( cols - i ) ) )
+		if( i == 0 || rows <= min_rows ) {
+			min_rows <- rows
+			min_row_i <- i
 		}
-		w <- append( w, paste( "grid", l, 1, "sticky=N" ) )
-		for( b in but[[ i ]] ) {
-			w <- append( w, getButton( b ) )
-		}
-		w <- append( w, "null pady=30" )
 	}
+	
+	stopifnot( min_rows > 0 )
+	w <- paste( "grid ", min_rows, " ", cols + 1, " relief=groove sticky=NEW", sep="" )
+	file_i <- 1
+	section_i <- 1
+	#iterate over each (i,j) position in button grid, insert file buttons on the left,
+	#then a center null widget, then section buttons on the right
+	for( j in 1:min_rows ) {
+		for( i in 1:(cols+1) ) {
+			if( i <= min_row_i ) {
+				if( file_i > length( file_but ) ) {
+					w <- append( w, "null padx=34" )
+				} else {
+					w <- append( w, getButton( file_but[[ file_i ]] ) )
+					file_i <- file_i + 1
+				}
+			} else if( i == ( min_row_i + 1 ) ) {
+				w <- append( w, "null padx=5" )
+			} else {
+				if( section_i > length( section_but ) ) {
+					w <- append( w, "null padx=20" )
+				} else {
+					w <- append( w, getButton( section_but[[ section_i ]] ) )
+					section_i <- section_i + 1
+				}
+			}
+		}
+	}
+	print( w )
 	return( w )
 }
 
@@ -282,48 +313,51 @@ createWin( c(
 "window name=presentwin",
 getMenus( talk ),
 
-# "grid 1 2 pady=\"0 5\"",
-# 	"grid 2 1 relief=groove",
-# 	"grid 1 2 sticky=E",
-# 	"label \"section:\" font=8 sticky=W",
-# 	"droplist name=section values=\"\" function=sectiondrop bg=skyblue width=15",
-# 	"grid 1 3",
-# 	"button name=prev text=\"< Prev\" bg=skyblue sticky=S function=setsection action=-1 width=7",
-# 	"button name=curr text=\"Restart\" bg=skyblue sticky=S function=setsection action=\"\" width=7",
-# 	"button name=next text=\"Next >\" bg=skyblue sticky=S function=setsection action=+1 width=7",
-# 
-# 	"grid 2 1 relief=groove padx=\"5 0\"",
-# 	"grid 1 3 sticky=E",
-# 	"label \"slide:\" font=\"8\" sticky=W",
-#  	"droplist name=slides values=\"\" function=sectiondrop bg=greenyellow width=9",
-# 	"label \"/ n\" name=slidecount font=\"8\" sticky=W",
-# 	"grid 1 2",
-# 	"button name=back text=\"< Back\" bg=greenyellow sticky=S function=prevSlide action=-1 width=10",
-# 	"button name=go text=\"> Go\" bg=greenyellow sticky=S function=nextSlide action=+1 width=10",
-# 
-# 	getButtons( talk ),
+"grid 1 2 pady=\"0 5\"",
+	"grid 1 1 relief=groove",
+	"grid 2 1 padx=3 pady=3",
+	"grid 1 2 sticky=E",
+	"label \"section:\" font=8 sticky=W",
+	"droplist name=section values=\"\" function=sectiondrop bg=skyblue width=15",
+	"grid 1 3",
+	"button name=prev text=\"< Prev\" bg=skyblue sticky=S function=setsection action=-1 width=7",
+	"button name=curr text=\"Restart\" bg=skyblue sticky=S function=setsection action=\"\" width=7",
+	"button name=next text=\"Next >\" bg=skyblue sticky=S function=setsection action=+1 width=7",
 
+	"grid 1 1 relief=groove padx=\"5 0\"",
+	"grid 2 1 padx=3 pady=3",
+	"grid 1 3 sticky=E",
+	"label \"slide:\" font=\"8\" sticky=W",
+ 	"droplist name=slides values=\"\" function=sectiondrop bg=greenyellow width=9",
+	"label \"/ n\" name=slidecount font=\"8\" sticky=W",
+	"grid 1 2",
+	"button name=back text=\"< Back\" bg=greenyellow sticky=S function=prevSlide action=-1 width=10",
+	"button name=go text=\"> Go\" bg=greenyellow sticky=S function=nextSlide action=+1 width=10",
 
-"grid 1 2",
 	getButtons( talk ),
-	"grid 2 1 borderwidth=0 sticky=N",
-		"grid 2 1 relief=groove sticky=EW padx=\"5 0\"",
-		"grid 1 3 sticky=E",
-		"label \"slide:\" font=\"8\" sticky=W",
-	 	"droplist name=slides values=\"\" function=sectiondrop bg=greenyellow width=9 function=slidedrop",
-		"label \"/ n\" name=slidecount font=\"8\" sticky=w",
-		"grid 1 2",
-		"button name=back text=\"< Back\" bg=greenyellow sticky=S function=prevSlide action=-1 width=11",
-		"button name=go text=\"> Go\" bg=greenyellow sticky=S function=nextSlide action=+1 width=11",
-	
-		"grid 2 1 relief=groove pady=\"5 0\" sticky=EW padx=\"5 0\"",
-		"grid 1 2 sticky=E",
-		"label \"section:\" font=8 sticky=W",
-		"droplist name=section values=\"\" function=sectiondrop bg=skyblue width=15",
-		"grid 1 3",
-		"button name=prev text=\"< Prev\" bg=skyblue sticky=S function=setsection action=-1 width=7",
-		"button name=curr text=\"Restart\" bg=skyblue sticky=S function=setsection action=\"\" width=7",
-		"button name=next text=\"Next >\" bg=skyblue sticky=S function=setsection action=+1 width=7",
+
+
+#side layout
+# "grid 1 2",
+# 	getButtons( talk ),
+# 	"grid 2 1 borderwidth=0 sticky=N",
+# 		"grid 2 1 relief=groove sticky=EW padx=\"5 0\"",
+# 		"grid 1 3 sticky=E",
+# 		"label \"slide:\" font=\"8\" sticky=W",
+# 	 	"droplist name=slides values=\"\" function=sectiondrop bg=greenyellow width=9 function=slidedrop",
+# 		"label \"/ n\" name=slidecount font=\"8\" sticky=w",
+# 		"grid 1 2",
+# 		"button name=back text=\"< Back\" bg=greenyellow sticky=S function=prevSlide action=-1 width=11",
+# 		"button name=go text=\"> Go\" bg=greenyellow sticky=S function=nextSlide action=+1 width=11",
+# 	
+# 		"grid 2 1 relief=groove pady=\"5 0\" sticky=EW padx=\"5 0\"",
+# 		"grid 1 2 sticky=E",
+# 		"label \"section:\" font=8 sticky=W",
+# 		"droplist name=section values=\"\" function=sectiondrop bg=skyblue width=15",
+# 		"grid 1 3",
+# 		"button name=prev text=\"< Prev\" bg=skyblue sticky=S function=setsection action=-1 width=7",
+# 		"button name=curr text=\"Restart\" bg=skyblue sticky=S function=setsection action=\"\" width=7",
+# 		"button name=next text=\"Next >\" bg=skyblue sticky=S function=setsection action=+1 width=7",
 
 
 ""
