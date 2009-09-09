f03ab4a73751f405374ddba57424873cbf6834c9
alex_google <alex_google@mofo.ca>
2009-09-09 16:26:48 +0000

    added auto inclusion of bwidget source for unix users (mostly for OSX) and added instructions for installing tktable with mac ports. Fixes issue 53.

--- PBSmodelling/R/zzz.r
+++ PBSmodelling/R/zzz.r
@@ -21,7 +21,7 @@ Pacific Biological Station, Nanaimo
 ")
 
 	#TODO find a better place
-	bwidget <- tclRequire("BWidget")
+	bwidget <- tclRequire("BWidget", warn = FALSE)
 	tktable <- tclRequire("Tktable")
 	if( is.logical( bwidget ) || is.logical( tktable ) ) {
 		cat( "-------------------------------------------------------------\n" )
@@ -32,12 +32,15 @@ Pacific Biological Station, Nanaimo
 		bwidget <- tclRequire("BWidget")
 		if( is.logical( bwidget ) ) {
 			cat( "ERROR: PBS Modelling requires the tcl package \"BWidget\"\nand cannot proceed until it is installed.\n" )
-			cat( "Ubuntu (apt) users can install via the command:\n\tapt-get install bwidget\n\n" )
+			cat( "Ubuntu (apt) users can install via the command:\n\tsudo apt-get install bwidget\n" )
+			cat( "Bwidget source can be downloaded from http://sourceforge.net/projects/tcllib/files/\n\n" )
 		}
 	}
 	if( is.logical( tktable ) ) {
 		cat( "ERROR: PBS Modelling requires the tcl package \"Tktable\"\nand cannot proceed until it is installed.\n" )
-		cat( "Ubuntu (apt) users can install via the command:\n\tapt-get install libtktable2.9\n\n" )
+		cat( "Ubuntu (apt) users can install via the command:\n\tsudo apt-get install libtktable2.9\n" )
+		cat( "Mac (port) users can install via the command:\n\tsudo port install tktable (assuming mac ports is installed)\n" )
+		cat( "tktable can also be donwloaded from http://sourceforge.net/projects/tktable/files/\n\n" )
 	}
 	if( is.logical( bwidget ) || is.logical( tktable ) ) {
 		cat( "-------------------------------------------------------------\n" )
