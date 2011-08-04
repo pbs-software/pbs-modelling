2d994e67a25577f29b14e662240636f926ffa847
rowan.haigh@dfo-mpo.gc.ca <rowan.haigh@dfo-mpo.gc.ca@effd1636-6b16-11de-a8c1-9da277d31688>
2011-08-04 20:51:18 +0000

    Version 2.62 for CRAN submission (if all goes well).

--- trunk/PBSmodelling/inst/PBStools/RPaths.bat
+++ trunk/PBSmodelling/inst/PBStools/RPaths.bat
@@ -5,14 +5,15 @@ if not defined PBS_SETLOCAL (
 
 rem ***** Edit the seven PATH variables listed below *****
 set R_Base=C:\WinApps\R
-set R_Root=%R_Base%\R-2.9.0
+set R_Root=%R_Base%\R-2.13.1
 set TOOLS_PATH=%R_Base%\Rtools\bin
 set PERL_PATH=%R_Base%\Rtools\perl\bin
 set MINGW_PATH=%R_Base%\Rtools\MinGW\bin
 set TEX_PATH=C:\WinApps\MiKTeX\miktex\bin
 set HTMLHELP_PATH=C:\Utils\HHW
 
-set R_PATH=%R_Root%\bin
+set R_PATH=%R_Root%\bin\i386
+set perlR=%R_Root%\share\perl\R
 
 rem ***** Edit environment variables below *****
 set R_PAPERSIZE=letter
@@ -21,3 +22,4 @@ set R_OSTYPE=windows
 rem ***** Specify special programs if you use them *****
 set xcopy=%windir%\system32\xcopy.exe
 rem set wzzip=C:\WinApps\WinZip\wzzip.exe
+
