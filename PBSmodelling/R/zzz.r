1a948dd76529ca95071c335b990b517108427bde
rowan.haigh <rowan.haigh@dfo-mpo.gc.ca>
2011-11-21 19:09:51 +0000

    Used 'packageStartupMessage' and 'message' rather than 'cat' in '.onLoad' in 'zzz.r'. Updated UG.

--- PBSmodelling/R/zzz.r
+++ PBSmodelling/R/zzz.r
@@ -12,43 +12,47 @@
 	
 	userguide_path <- system.file( "doc/PBSmodelling-UG.pdf", package = "PBSmodelling" )
 	
-	cat("
-PBS Modelling", pkg_info$Version, "-- Copyright (C) 2005-2010 Fisheries and Oceans Canada
+	packageStartupMessage("
+-----------------------------------------------------------
+PBS Modelling ", pkg_info$Version, " -- Copyright (C) 2005-2011 Fisheries and Oceans Canada
 
 A complete user guide 'PBSmodelling-UG.pdf' is located at 
 ", userguide_path, "
 
-Packaged on", pkg_date, "
+Packaged on ", pkg_date, "
 Pacific Biological Station, Nanaimo
+-----------------------------------------------------------
+
 
 ")
 	#Load custom PBSmodelling tcl scripts
 	tcl("lappend", "auto_path", system.file( "tcl_scripts", package = "PBSmodelling" ) )
 	tclRequire( "PBSmodelling" )
 
-	#TODO find a better place
+	#TO DO find a better place
 	bwidget <- tclRequire("BWidget", warn = FALSE)
 	tktable <- tclRequire("Tktable")
 	if( is.logical( bwidget ) || is.logical( tktable ) ) {
-		cat( "-------------------------------------------------------------\n" )
+		message("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n" )
 	}
 	if( is.logical( bwidget ) ) {
 		#try included distribution
 		tcl("lappend", "auto_path", system.file( "thirdparty/BWidget-1.9.0/", package = "PBSmodelling" ) )
 		bwidget <- tclRequire("BWidget")
 		if( is.logical( bwidget ) ) {
-			cat( "ERROR: PBS Modelling requires the tcl package \"BWidget\"\nand cannot proceed until it is installed.\n" )
-			cat( "Ubuntu (apt) users can install via the command:\n\tsudo apt-get install bwidget\n" )
-			cat( "Bwidget source can be downloaded from http://sourceforge.net/projects/tcllib/files/\n\n" )
+		message("ERROR: PBSmodelling requires the tcl package \"BWidget\"\nand cannot proceed until it is installed.\n" ,
+				"Ubuntu (apt) users can install via the command:\n\tsudo apt-get install bwidget\n",
+				"Bwidget source can be downloaded from\n\thttp://sourceforge.net/projects/tcllib/files/\n")
 		}
 	}
 	if( is.logical( tktable ) ) {
-		cat( "ERROR: PBS Modelling requires the tcl package \"Tktable\"\nand cannot proceed until it is installed.\n" )
-		cat( "Ubuntu (apt) users can install via the command:\n\tsudo apt-get install libtktable2.9\n" )
-		cat( "Mac (port) users can install via the command:\n\tsudo port install tktable (assuming mac ports is installed)\n" )
-		cat( "tktable can also be donwloaded from http://sourceforge.net/projects/tktable/files/\n\n" )
+		message("ERROR: PBSmodelling requires the tcl package \"Tktable\"\nand cannot proceed until it is installed.\n",
+			"Ubuntu (apt) users can install via the command:\n\tsudo apt-get install libtktable2.10\n",
+			"Mac (port) users can install via the command:\n\tsudo port install tktable\n",
+			"assuming Mac Ports is installed (http://www.macports.org/)\n",
+			"tktable can also be donwloaded from \n\thttp://sourceforge.net/projects/tktable/files/\n" )
 	}
 	if( is.logical( bwidget ) || is.logical( tktable ) ) {
-		cat( "-------------------------------------------------------------\n" )
+		message("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n")
 	}
 }
